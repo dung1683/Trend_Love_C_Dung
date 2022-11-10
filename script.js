9f9c43e0d7c6a84d9b1081620d95628d01b14b1f
dung1683 <60754223+dung1683@users.noreply.github.com>
2022-11-10 17:32:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ setTimeout(function(){
             break;
         }
     }while(true);
-  }, 14000);
+  }, 18000);
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ['#fff'];
   var visible = true;
